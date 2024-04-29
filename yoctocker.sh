303f405dc505ef398c768ef6ba6cdda656c60fa8
Quentin Dufournet <qdu.dev.pro@gmail.com>
2024-04-29 13:20:30 +0200

    Fix ssh path in shell script

--- yoctocker.sh
+++ yoctocker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -it --workdir /home/root/ -v $1/:/home/root/ -v /home/$USER/.ssh/:/home/root/.ssh/ yoctocker:latest
+docker run -it --workdir /home/root/ -v $1/:/home/root/ -v /home/$USER/.ssh/:/home/yoctocker/.ssh/ yoctocker:latest
